2b98d0eaffbcf460eabe2ab5307466e9087fafc8
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-08-12 21:30:51 +0900

    라우터 경로 수정

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", router);
+app.use("/api", router);
 
 // DB에 연결 = sync() 사용으로 DB 모델(./models)의 변화를 감지하여 동기화
 const { sequelize } = require("./models");
