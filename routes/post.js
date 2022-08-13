a218289daf0171a064ae5b4fc33841e105990a0a
JJooonji <korewa9@naver.com>
2022-08-13 17:00:23 +0900

    게시글 전체 조회_api경로 변경으로 user 페이지로 이동

--- routes/post.js
+++ routes/post.js
@@ -3,29 +3,18 @@ const { Post, User } = require("../models")
 const authMiddleware = require("../middlewares/auth-middleware")
 const router = express.Router();
 
-//게시글 전체 조회
-router.get("/", async (req, res) => {
-    const posts = await Post.findAll({
-        order : [["updatedAt", "DESC"]],//updatedAt으로 내림차순 정렬
-    });
-    res.send({ posts })
-});
-
-
 //게시글 생성
-router.post("/posts", authMiddleware, async (req, res) => {
+router.post("/", authMiddleware, async (req, res) => {
     const { user } = res.locals
-    
-    // console.log(user, "아이디확인")
 
-    const { title, content, url } = req.body;
+    const { title, content, url } = req.body; //title, content, url r값만 입력
 
-    if(!content || !title) {
+    if(!content || !title) {//content 또는 title 값이 없으면!
         return res.send({message: "제목 또는 내용을 작성해주세요."})
     }
     const createPost = await Post.create({
-        userId:user.userId,
-        nickname:user.nickname,
+        userId:user.userId,//user페이지에서 userId를 받아옴
+        nickname:user.nickname,//user 페이지에서 nickname을 받아서
         title, 
         content, 
         url
