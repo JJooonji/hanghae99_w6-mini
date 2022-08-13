be96663cf6496e26d396736e3e16721f862dfcd1
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-08-13 12:23:58 +0900

    auth-middleware 추가, user router 입력

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const commentRouter = require('./comment');
-const postRouter = require('./post');
+// const commentRouter = require('./comment');
+// const postRouter = require('./post');
 const userRouter = require('./user');
 
-router.use('/comments', commentRouter);
-router.use('/posts', postRouter);
-router.use('/users', userRouter);
+// router.use('/comments', commentRouter);
+// router.use('/posts', postRouter);
+router.use('/', userRouter);
 
 module.exports = router;
