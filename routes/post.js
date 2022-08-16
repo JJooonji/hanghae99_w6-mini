0b5fa64c1db2c402f56e3b52bdfea648027f0332
JJooonji <korewa9@naver.com>
2022-08-16 15:24:07 +0900

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -34,58 +34,58 @@ router.post("/", async (req, res) => {
 });
 
 //게시글 수정
-router.put("/:postId", async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const { title, content, url } = req.body;
+// router.put("/:postId", async (req, res) => {
+//   try {
+//     const { postId } = req.params;
+//     const { title, content, url } = req.body;
 
-    const changePost = await Post.findOne({ where: { postId } });
+//     const changePost = await Post.findOne({ where: { postId } });
 
-    if (!changePost) {
-      return res.status(400).json({ message: "해당 게시글이 없습니다." });
-    }
+//     if (!changePost) {
+//       return res.status(400).json({ message: "해당 게시글이 없습니다." });
+//     }
 
-    if(!content){
-      return res.status(400).json({ message : "게시글 내용을 작성해주세요."})
-    }
+//     if(!content){
+//       return res.status(400).json({ message : "게시글 내용을 작성해주세요."})
+//     }
 
-    // const { user } = await res.locals;
-    // if (user.nickname !== changePost.nickname) {
-    //   return res.json({ message: "수정 권한이 없습니다." });
-    // }
+//     // const { user } = await res.locals;
+//     // if (user.nickname !== changePost.nickname) {
+//     //   return res.json({ message: "수정 권한이 없습니다." });
+//     // }
 
-    await Post.update({ title, content, url }, { where: { postId } });
-    res.status(201).json({ message: "게시글을 수정하였습니다." });
-  } catch (error) {
-    console.error(err);
-    res.status(400).send({ errorMessage: "error" });
-  }
-});
+//     await Post.update({ title, content, url }, { where: { postId } });
+//     res.status(201).json({ message: "게시글을 수정하였습니다." });
+//   } catch (error) {
+//     console.error(err);
+//     res.status(400).send({ errorMessage: "error" });
+//   }
+// });
 
-//게시글 삭제
-router.delete("/:postId",  async (req, res) => {
-  try {
-    const { postId } = req.params;
+// //게시글 삭제
+// router.delete("/:postId",  async (req, res) => {
+//   try {
+//     const { postId } = req.params;
 
-    const findPost = await Post.findOne({ where: { postId } });
-    if (!findPost) {
-      return res.status(400).json({ message: "해당 게시글이 없습니다." });
-    }
+//     const findPost = await Post.findOne({ where: { postId } });
+//     if (!findPost) {
+//       return res.status(400).json({ message: "해당 게시글이 없습니다." });
+//     }
 
-    // const { user } = await res.locals;
-    // if (user.nickname !== findPost.nickname) {
-    //   return res.status(400).json({ message: "삭제 권한이 없습니다." });
-    // }
+//     // const { user } = await res.locals;
+//     // if (user.nickname !== findPost.nickname) {
+//     //   return res.status(400).json({ message: "삭제 권한이 없습니다." });
+//     // }
 
-    const removePost = await Post.destroy({where : {postId}});
-    if(removePost) {
-        await Comment.destroy({where: {postId: req.params.postId}})
-        res.status(200).json({ postId, message: "게시글을 삭제하였습니다."  });
-    }
-  } catch (error) {
-    console.error(err);
-    res.status(400).send({ errorMessage: "error" });
-  }
-});
+//     const removePost = await Post.destroy({where : {postId}});
+//     if(removePost) {
+//         await Comment.destroy({where: {postId: req.params.postId}})
+//         res.status(200).json({ postId, message: "게시글을 삭제하였습니다."  });
+//     }
+//   } catch (error) {
+//     console.error(err);
+//     res.status(400).send({ errorMessage: "error" });
+//   }
+// });
 
 module.exports = router;
